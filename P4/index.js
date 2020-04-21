401db892204e3052ef211e304ac6ce9b017ecb09
rgarciam6 <rebeca.gm1996@gmail.com>
2020-04-21 10:22:09 +0200

    intentando mejora manual y automático

--- P4/index.js
+++ P4/index.js
@@ -27,47 +27,60 @@ imagen.width = 150;
 imagen.height = 100;
 
 
+const fuente1 = document.getElementById("fuente1");
+const fuente2 = document.getElementById("fuente2");
+const fuente3 = document.getElementById("fuente3");
+const fuente4 = document.getElementById("fuente4");
 
+if (manual.onclick = () => {
+  fuente1.onclick = () => {
+    console.log("Fuente1");
+    video.src = video1.src;
+    video.currentTime = video1.currentTime;
+    video1.style.border = "4px solid red"
+    video2.style.border = "0"
+    video3.style.border = "0"
+    imagen.style.border = "0"
+    video.play();
 
-//-- Función de retrollamada de los botones de las fuentes
-fuente1.onclick = () => {
-  console.log("Fuente1");
-  video.src = video1.src;
-  video.currentTime = video1.currentTime;
-  video1.style.border = "4px solid red"
-  video2.style.border = "0"
-  video3.style.border = "0"
-  imagen.style.border = "0"
-  video.play();
+  };
+  fuente2.onclick = () => {
+    console.log("Fuente2");
+    video.src = video2.src;
+    video.currentTime = video2.currentTime;
+    video1.style.border = "0";
+    video2.style.border = "4px solid red";
+    video3.style.border = "0";
+    imagen.style.border = "0";
+    video.play();
 
-};
-fuente2.onclick = () => {
-  console.log("Fuente2");
-  video.src = video2.src;
-  video.currentTime = video2.currentTime;
-  video1.style.border = "0";
-  video2.style.border = "4px solid red";
-  video3.style.border = "0";
-  imagen.style.border = "0";
-  video.play();
+  };
+  fuente3.onclick = () => {
+    console.log("Fuente3");
+    video.src = video3.src;
+    video.currentTime = video3.currentTime;
+    video1.style.border = "0";
+    video2.style.border = "0";
+    video3.style.border = "4px solid red";
+    imagen.style.border = "0";
+    video.play();
+  };
+  fuente4.onclick = () => {
+    console.log("Fuente4");
+    video.src = null;
+    video.poster = imagen.src;
+    video1.style.border = "0";
+    video2.style.border = "0";
+    video3.style.border = "0";
+    imagen.style.border = "4px solid red";
+  };
 
-};
-fuente3.onclick = () => {
-  console.log("Fuente3");
-  video.src = video3.src;
-  video.currentTime = video3.currentTime;
-  video1.style.border = "0";
-  video2.style.border = "0";
-  video3.style.border = "4px solid red";
-  imagen.style.border = "0";
-  video.play();
-};
-fuente4.onclick = () => {
-  console.log("Fuente4");
-  video.src = null;
-  video.poster = imagen.src;
-  video1.style.border = "0";
-  video2.style.border = "0";
-  video3.style.border = "0";
-  imagen.style.border = "4px solid red";
-};
+});
+
+if (automatico.onclick = () =>{
+  video.poster="https://cdn.pixabay.com/photo/2015/03/15/09/35/intermission-674178_1280.png";
+  fuente1.onclick = null;
+  fuente2.onclick = null;
+  fuente3.onclick = null;
+  fuente4.onclick = null;
+});
