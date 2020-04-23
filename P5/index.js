c83be78a63a093eb24ef54caad6348c47e1e0824
rgarciam6 <rebeca.gm1996@gmail.com>
2020-04-23 09:06:06 +0200

    Funciona filtro color y filtro de grises. Botón original devuelve a inic

---
+++ P5/index.js
@@ -0,0 +1,114 @@
+console.log("Ejecutando JS....")
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+//-- Acceso al deslizador
+const deslizador_R = document.getElementById('deslizador_R');
+const deslizador_G = document.getElementById('deslizador_G');
+const deslizador_B = document.getElementById('deslizador_B');
+const deslizador_Gray = document.getElementById('deslizador_Gray');
+
+//-- Valor del deslizador
+const range_value_R = document.getElementById('range_value_R');
+const range_value_G = document.getElementById('range_value_G');
+const range_value_B = document.getElementById('range_value_B');
+const range_value_Gray = document.getElementById('range_value_Gray');
+
+//--Botones
+const gray = document.getElementById('gray');
+const original = document.getElementById('original')
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+img.onload = function () {
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  console.log("Imagen lista...");
+};
+
+
+function colors(){
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Mostrar el nuevo valor de los deslizadores
+  range_value_R.innerHTML = deslizador_R.value;
+  range_value_G.innerHTML = deslizador_G.value;
+  range_value_B.innerHTML = deslizador_B.value;
+
+  //-- Obtener el umbral de los colores de los deslizadores
+  var umbral_R = deslizador_R.value;
+  var umbral_G = deslizador_G.value;
+  var umbral_B = deslizador_B.value;
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    if (data[i] > umbral_R)
+      data[i] = umbral_R;
+    if (data[i+1] > umbral_G)
+      data[i+1] = umbral_G;
+    if (data[i+2] > umbral_B)
+      data[i+2] = umbral_B;
+    }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+//-- Funcion de retrollamada de los deslizadores
+deslizador_R.oninput = () => {
+  colors();
+}
+deslizador_G.oninput = () => {
+  colors();
+}
+deslizador_B.oninput = () => {
+  colors();
+}
+
+gray.onclick = () => {
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    var R = data[i];
+    var G = data[i+1];
+    var B = data[i+2];
+    var gris = (3 * R + 4 * G + B)/8;
+    gris = data[i] = data[i+1] = data[i+2];
+    }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+original.onclick = () => {
+    ctx.drawImage(img, 0,0);
+    deslizador_R.value = 255;
+    range_value_R.innerHTML = deslizador_R.value;
+    deslizador_G.value = 255;
+    range_value_G.innerHTML = deslizador_G.value;
+    deslizador_B.value = 255;
+    range_value_B.innerHTML = deslizador_B.value;
+
+}
+
+console.log("Fin...");
